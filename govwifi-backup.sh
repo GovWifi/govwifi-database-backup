aefe316f23d4b9022f254d365873a7a50b21fce2
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2026-06-23 18:41:08 +0100

    MySQL Backups Recently Started Failing
    Fix by changing SSL mode
    
    Adjust
    
    Redo

--- govwifi-backup.sh
+++ govwifi-backup.sh
@@ -14,6 +14,7 @@ echo "Starting encrypted backup of databases to S3 at `date`..."
 # set the mysql pass pre command inline so not to appear in the proc list
 echo -n "STARTING SQL DUMP OF SESSIONS DB - "
 MYSQL_PWD="${WIFI_DB_PASS}" mysqldump -h "${WIFI_DB_HOSTNAME}" -u "${WIFI_DB_USER}" \
+  --ssl \
   --compress --quick --single-transaction "${WIFI_DB_NAME}" \
   | gzip -c | gpg --symmetric --batch --yes --passphrase ${ENCRYPTION_KEY} | aws ${BACKUP_ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/govwifi-backup-sessions-${STAMP_DATE}.sql.gz.gpg"
 STATUS1=$?
@@ -21,6 +22,7 @@ STATUS1=$?
 
 echo -n "STARTING SQL DUMP OF USERS DB - "
 MYSQL_PWD="${USERS_DB_PASS}" mysqldump -h "${USERS_DB_HOSTNAME}" -u "${USERS_DB_USER}" \
+  --ssl \
   --compress --quick --single-transaction "${USERS_DB_NAME}" \
   | gzip -c | gpg --symmetric --batch --yes --passphrase ${ENCRYPTION_KEY} | aws ${BACKUP_ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/govwifi-backup-user-details-${STAMP_DATE}.sql.gz.gpg"
 STATUS2=$?
@@ -28,6 +30,7 @@ STATUS2=$?
 
 echo -n "STARTING SQL DUMP OF ADMIN DB - "
 MYSQL_PWD="${ADMIN_DB_PASS}" mysqldump -h "${ADMIN_DB_HOSTNAME}" -u "${ADMIN_DB_USER}" \
+  --ssl \
   --compress --quick --single-transaction "${ADMIN_DB_NAME}" \
   | gzip -c | gpg --symmetric --batch --yes --passphrase ${ENCRYPTION_KEY} | aws ${BACKUP_ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/govwifi-backup-admin-${STAMP_DATE}.sql.gz.gpg"
 STATUS3=$?
