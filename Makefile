39f5ea423c8b8c9442d37423804a75abef65171e
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2026-06-24 13:13:47 +0100

    fix: [GW-2857] Add skip ssl check as we are in a secure VPC

--- Makefile
+++ Makefile
@@ -1,15 +1,14 @@
 build:
-	docker-compose down
-	docker-compose up --build -d db fake-s3
-	docker-compose exec -T fake-s3 awslocal s3 mb s3://backup-bucket &> /dev/null
-	./mysql/wait_for_services & wait
-	docker-compose up app
+	docker compose down
+	docker compose up --build -d db fake-s3
+	docker compose exec -T fake-s3 awslocal s3 mb s3://backup-bucket &> /dev/null
+	docker compose up app
 
 test: build
 	./tests/test_backup_files_exist
 
 destroy:
-	docker-compose down --volumes
+	docker compose down --volumes
 
 .PHONY: build test destroy
-	
+
--- govwifi-backup.sh
+++ govwifi-backup.sh
@@ -8,13 +8,14 @@ else
   BACKUP_ENDPOINT_ARG="";
 fi
 
-STAMP_DATE=`date +"%Y-%m-%d-%H-%M"`
+STAMP_DATE=$(date +"%Y-%m-%d-%H-%M")
 echo "Starting encrypted backup of databases to S3 at `date`..."
 
 # set the mysql pass pre command inline so not to appear in the proc list
 echo -n "STARTING SQL DUMP OF SESSIONS DB - "
 MYSQL_PWD="${WIFI_DB_PASS}" mysqldump -h "${WIFI_DB_HOSTNAME}" -u "${WIFI_DB_USER}" \
   --compress --quick --single-transaction "${WIFI_DB_NAME}" \
+  --ssl-verify-server-cert=FALSE \
   | gzip -c | gpg --symmetric --batch --yes --passphrase ${ENCRYPTION_KEY} | aws ${BACKUP_ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/govwifi-backup-sessions-${STAMP_DATE}.sql.gz.gpg"
 STATUS1=$?
 [ $STATUS1 -eq 0 ] && echo COMPLETE || echo FAILED
@@ -22,6 +23,7 @@ STATUS1=$?
 echo -n "STARTING SQL DUMP OF USERS DB - "
 MYSQL_PWD="${USERS_DB_PASS}" mysqldump -h "${USERS_DB_HOSTNAME}" -u "${USERS_DB_USER}" \
   --compress --quick --single-transaction "${USERS_DB_NAME}" \
+  --ssl-verify-server-cert=FALSE \
   | gzip -c | gpg --symmetric --batch --yes --passphrase ${ENCRYPTION_KEY} | aws ${BACKUP_ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/govwifi-backup-user-details-${STAMP_DATE}.sql.gz.gpg"
 STATUS2=$?
 [ $STATUS2 -eq 0 ] && echo COMPLETE || echo FAILED
@@ -29,14 +31,15 @@ STATUS2=$?
 echo -n "STARTING SQL DUMP OF ADMIN DB - "
 MYSQL_PWD="${ADMIN_DB_PASS}" mysqldump -h "${ADMIN_DB_HOSTNAME}" -u "${ADMIN_DB_USER}" \
   --compress --quick --single-transaction "${ADMIN_DB_NAME}" \
+  --ssl-verify-server-cert=FALSE \
   | gzip -c | gpg --symmetric --batch --yes --passphrase ${ENCRYPTION_KEY} | aws ${BACKUP_ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/govwifi-backup-admin-${STAMP_DATE}.sql.gz.gpg"
 STATUS3=$?
 [ $STATUS3 -eq 0 ] && echo COMPLETE || echo FAILED
 
 if [ $STATUS1 -ne 0 ] || [ $STATUS2 -ne 0 ] || [ $STATUS3 -ne 0 ]
 then
-        echo BACKUPS FAILED - `date`
+        echo "BACKUPS FAILED - $(date)"
         exit 1
 fi
 
-echo BACKUPS OK - `date`
+echo "BACKUPS OK - $(date)"
